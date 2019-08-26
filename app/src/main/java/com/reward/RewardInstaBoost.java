23fec7bbedac674e433d807660831788c4aebb6a
Tapbi_DatDB <datdb@tapbitech.com>
2019-08-26 13:51:39 +0700

    delay time

--- app/src/main/java/com/reward/RewardInstaBoost.java
+++ app/src/main/java/com/reward/RewardInstaBoost.java
@@ -2,12 +2,18 @@ package com.reward;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 
 public class RewardInstaBoost {
-    public static void success(Context context, String pka) {
-        Intent intent = new Intent();
-        intent.setAction("what.blf.okela");
-        intent.putExtra("package_name", pka);
-        context.sendBroadcast(intent);
+    public static void success(final Context context, final String pka, long delay) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent();
+                intent.setAction("what.blf.okela");
+                intent.putExtra("package_name", pka);
+                context.sendBroadcast(intent);
+            }
+        }, delay);
     }
 }
