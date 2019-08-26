32788240419be4b6f48a6baf762b14702a346ed3
Tapbi_DatDB <datdb@tapbitech.com>
2019-08-26 13:58:28 +0700

    foreground

--- app/src/main/java/com/reward/RewardInstaBoost.java
+++ app/src/main/java/com/reward/RewardInstaBoost.java
@@ -1,19 +1,31 @@
 package com.reward;
 
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 
+import static android.app.ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND;
+import static android.app.ActivityManager.RunningAppProcessInfo.IMPORTANCE_VISIBLE;
+
 public class RewardInstaBoost {
-    public static void success(final Context context, final String pka, long delay) {
+    public void success(final Context context, final String pka, long delay) {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent();
-                intent.setAction("what.blf.okela");
-                intent.putExtra("package_name", pka);
-                context.sendBroadcast(intent);
+                if (foregrounded()) {
+                    Intent intent = new Intent();
+                    intent.setAction("what.blf.okela");
+                    intent.putExtra("package_name", pka);
+                    context.sendBroadcast(intent);
+                }
             }
         }, delay);
+    }
+
+    private boolean foregrounded() {
+        ActivityManager.RunningAppProcessInfo appProcessInfo = new ActivityManager.RunningAppProcessInfo();
+        ActivityManager.getMyMemoryState(appProcessInfo);
+        return (appProcessInfo.importance == IMPORTANCE_FOREGROUND || appProcessInfo.importance == IMPORTANCE_VISIBLE);
     }
 }
